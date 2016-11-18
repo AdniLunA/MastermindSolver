b6a817c58f5cf9f9d8ac6cc1abc5b9dcdffeb1bd
AdniLunA <linda.latreider@web.de>
2016-11-18 23:15:23 +0100

    structural changes (javafx fixes)

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,4 +1,9 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Mastermind extends Application{
@@ -6,14 +11,20 @@ public class Mastermind extends Application{
 
 
     //functions
-    public void start(Stage stage){
-        //todo
+    @Override
+    public void start (Stage primaryStage) throws Exception {
+        System.out.println("Mastermind - start");
+        primaryStage.setTitle("Mastermind Solver");
+
+        primaryStage.show();
     }
+
     //getter + setter
 
     //MAIN
-    public static void main (String ... args){
+    public static void main (String ... args) {
         System.out.println("Mastermind - main");
+        launch(args);
     }
 }
 
