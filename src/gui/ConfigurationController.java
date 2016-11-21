f5c401b6f15f051e62e5304a48df845aef4ce886
Linda <linda.latreider@web.de>
2016-11-21 10:01:23 +0100

    Working Configure Settings

--- src/gui/ConfigurationController.java
+++ src/gui/ConfigurationController.java
@@ -1,13 +1,20 @@
 package gui;
 
 import config.Configuration;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 //import evolution.NumChromosome;
 //import evolution.IChromosome;
 
-public class ConfigurationController {
+public class ConfigurationController implements Initializable{
     //attributes
     private int lengthOfCode = Configuration.INSTANCE.DEFAULT_LENGTH_OF_CODE;
     private int numberOfColors = Configuration.INSTANCE.DEFAULT_NUMBER_OF_COLORS;
@@ -17,6 +24,14 @@ public class ConfigurationController {
     TextField loc_textfield;
     @FXML
     Slider loc_slider;
+    @FXML
+    TextField noc_textfield;
+    @FXML
+    Slider noc_slider;
+    @FXML
+    TextField not_textfield;
+    @FXML
+    Slider not_slider;
 
     //functions
     private int checkBoundaries(String sValue, int maxValue){
@@ -37,20 +52,50 @@ public class ConfigurationController {
         }
     }
 
-    public void onchangeLengthOfCodeTextfield(){
-        System.out.println("ConfigurationController - onchangeLengthOfCodeTextfield");
+    @FXML
+    private void onchangeLOCTextfield(){
+        System.out.println("ConfigurationController - onchangeLOCTextfield");
 
         int newValue = checkBoundaries(loc_textfield.getText(), Configuration.INSTANCE.MAX_LENGTH_OF_CODE);
         if (newValue != 0) {
             loc_textfield.setStyle("-fx-text-inner-color: black;");
             lengthOfCode = newValue;
             loc_slider.setValue((double) newValue);
-            loc_textfield.setText(""+newValue);
+            loc_textfield.setText("" + newValue);
         } else {
             loc_textfield.setStyle("-fx-text-inner-color: red;");
         }
     }
 
+    @FXML
+    private void onchangeNOCTextfield(){
+        System.out.println("ConfigurationController - onchangNOCTextfield");
+
+        int newValue = checkBoundaries(noc_textfield.getText(), Configuration.INSTANCE.MAX_NUMBER_OF_COLORS);
+        if (newValue != 0) {
+            noc_textfield.setStyle("-fx-text-inner-color: black;");
+            numberOfColors = newValue;
+            noc_slider.setValue((double) newValue);
+            noc_textfield.setText("" + newValue);
+        } else {
+            noc_textfield.setStyle("-fx-text-inner-color: red;");
+        }
+    }
+
+    @FXML
+    private void onchangeNOTTextfield(){
+        System.out.println("ConfigurationController - onchangeNOTTextfield");
+
+        int newValue = checkBoundaries(not_textfield.getText(), Configuration.INSTANCE.MAX_NUMBER_OF_TRIES);
+        if (newValue != 0) {
+            not_textfield.setStyle("-fx-text-inner-color: black;");
+            numberOfTries = newValue;
+            not_slider.setValue((double) newValue);
+            not_textfield.setText("" + newValue);
+        } else {
+            not_textfield.setStyle("-fx-text-inner-color: red;");
+        }
+    }
 
     public void saveSettings(){
         System.out.println("Configuration Page - saveSettings");
@@ -59,13 +104,6 @@ public class ConfigurationController {
     public void generateCodeSettingField(){
         System.out.println("Configuration Page - generateCodeSettingField");
     }
-
-    /*
-    public IChromosome onclickGetRandomCode(){
-        System.out.println("Configuration Page - onclickGetRandomCode");
-        return new NumChromosome();
-    }
-    */
 
     public void onclickStartSimulation(){
         System.out.println("Configuration Page - onclickStartSimulation");
@@ -94,5 +132,36 @@ public class ConfigurationController {
 
     public void setNumberOfTries(int numberOfTries) {
         this.numberOfTries = numberOfTries;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.loc_slider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                        loc_textfield.setText(String.valueOf(newValue.intValue()));
+                        loc_textfield.setStyle("-fx-text-inner-color: black;");
+                    }
+                }
+        );
+        this.noc_slider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                        noc_textfield.setText(String.valueOf(newValue.intValue()));
+                        noc_textfield.setStyle("-fx-text-inner-color: black;");
+                    }
+                }
+        );
+        this.not_slider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                        not_textfield.setText(String.valueOf(newValue.intValue()));
+                        not_textfield.setStyle("-fx-text-inner-color: black;");
+                    }
+                }
+        );
     }
 }
